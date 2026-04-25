292e09a9207abbba5b9ef3f9dbe9f05d5492d919
H Merve Arslan <59476349+hmervearslan@users.noreply.github.com>
2026-04-25 20:41:24 +0300

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,32 +1,28 @@
-
 def fibonacci(m):
-    m = int(m)
-    def fibonaccii(a,b,m,result):
-        c = a+b
-        result.append(c)
-        if c < m:
-            fibonaccii(b,c,m,result)
-        return result
-    return fibonaccii(0,1,m,[])
+    a, b = 0, 1
+    result = []
+    while b < m:
+        result.append(b)
+        a, b = b, a + b
+    return result
 
 def prime(n): 
-    if n <= 0: 
-        return "what?" 
-    elif n == 1: 
-        return "it is 1" 
-    for i in range(2, n): 
-        if n%i == 0: 
-            return "not a prime" 
-    return "prime" 
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True 
  
  
 def list_prime(list): 
 	prime_list =[ ] 
 	for i in list: 
 		x = prime(i) 
-		if x == "prime": 
+		if x == True: 
 			prime_list.append(i) 
 	return prime_list 
 
 # command that find prime numbers is Fibonacci sequence from 0 to m	 
+m =int(input("find prime numbers is Fibonacci sequence from 0 to m. What is the m you want?: "))
 print(list_prime(fibonacci(m))) 
